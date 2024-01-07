7fae832b6bbb7fa92d9107db771c47983fc9d008
owoDra <85442566+owoDra@users.noreply.github.com>
2024-01-08 03:55:35 +0900

    Tagの追加

--- Source/GESAddon/GESAddon/GameplayTag/GESATags_Stat.cpp
+++ Source/GESAddon/GESAddon/GameplayTag/GESATags_Stat.cpp
@@ -7,3 +7,4 @@
 // Stat.Equipment.Skill
 
 UE_DEFINE_GAMEPLAY_TAG(TAG_Stat_Equipment_Skill_Stock, "Stat.Equipment.Skill.Stock");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Stat_Equipment_Skill_Point, "Stat.Equipment.Skill.Point");
--- Source/GESAddon/GESAddon/GameplayTag/GESATags_Stat.h
+++ Source/GESAddon/GESAddon/GameplayTag/GESATags_Stat.h
@@ -9,3 +9,4 @@
 // Stat.Equipment.Skill
 
 GESADDON_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Stat_Equipment_Skill_Stock);
+GESADDON_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Stat_Equipment_Skill_Point);
